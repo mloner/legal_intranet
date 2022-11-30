757fafa877390a420770257c940b1d3842923526
LoiIver <LoiIver@users.noreply.github.com>
2022-11-30 15:12:50 +0300

    UTG-2190  adjust access rights

--- src/Utg.LegalService.BL/Features/AccessRights/Get/GetTaskAccessRightsCommandHandler.cs
+++ src/Utg.LegalService.BL/Features/AccessRights/Get/GetTaskAccessRightsCommandHandler.cs
@@ -12,7 +12,7 @@ using Utg.LegalService.Common.Models.Client.Task;
 
 namespace Utg.LegalService.BL.Features.AccessRights.Get;
 
-public class GetTaskAccessRightsCommandHandler 
+public class GetTaskAccessRightsCommandHandler
     : IRequestHandler<GetTaskAccessRightsCommand, Result<TaskAccessRights>>
 {
     private readonly ILogger<GetTaskAccessRightsCommandHandler> _logger;
@@ -24,7 +24,7 @@ public class GetTaskAccessRightsCommandHandler
     }
 
     public async System.Threading.Tasks.Task<Result<TaskAccessRights>> Handle(
-        GetTaskAccessRightsCommand command, 
+        GetTaskAccessRightsCommand command,
         CancellationToken cancellationToken)
     {
         try
@@ -32,13 +32,13 @@ public class GetTaskAccessRightsCommandHandler
             var ar = new TaskAccessRights
             {
                 IsPerformerAvailable = IsPerformerAvailable(command.AuthInfo),
-                CanShowDetails = CanShowDetails(command.AuthInfo),
+                CanShowDetails = CanShowDetails(command.Task, command.AuthInfo),
                 CanEdit = CanEdit(command.Task, command.AuthInfo),
                 CanDelete = CanDelete(command.Task, command.AuthInfo),
                 CanMakeReport = CanMakeReport(command.Task, command.AuthInfo),
                 CanPerform = CanPerform(command.Task, command.AuthInfo),
                 CanReview = CanReview(command.Task, command.AuthInfo),
-                CanSelfAssignTask = CanSelfAssignTask(command.Task, command.AuthInfo) 
+                CanSelfAssignTask = CanSelfAssignTask(command.Task, command.AuthInfo)
             };
 
             return Result<TaskAccessRights>.Ok(ar);
@@ -47,34 +47,32 @@ public class GetTaskAccessRightsCommandHandler
         {
             var failMsg = "Failed to get task access rights.";
             _logger.LogError(e, "{@Msg} {@Command}", failMsg, command);
-            
+
             return Result<TaskAccessRights>.Internal(failMsg);
         }
     }
 
     private static bool CanSelfAssignTask(TaskModel task, AuthInfo authInfo)
-// если я - исполнитель И  у задачи нет исполнителя и она из тех трёх типов, исполнителя по которым назначаем сами
-        => authInfo.Roles.Contains((int)Role.LegalPerformer) 
+        // если я - исполнитель И  у задачи нет исполнителя и она из тех трёх типов, исполнителя по которым назначаем сами
+        => authInfo.Roles.Contains((int)Role.LegalPerformer)
                 && !task.PerformerUserProfileId.HasValue && StaticData.TypesToSelfAssign.Contains(task.Type);
 
     private static bool IsPerformerAvailable(AuthInfo authInfo)
     {
         return authInfo.Roles.Contains((int)Role.LegalHead);
     }
-    
-    private static bool CanShowDetails(AuthInfo authInfo)
+
+    private static bool CanShowDetails(TaskModel model, AuthInfo authInfo)
     {
-        return new int[] { (int)Role.LegalHead, (int)Role.IntranetUser, (int)Role.LegalPerformer }
+        return new int[] { (int)Role.LegalHead, (int)Role.LegalPerformer }
             .Intersect(authInfo.Roles)
-            .Any();
+            .Any() || authInfo.UserProfileId == model.AuthorUserProfileId;
     }
     
     private static bool CanEdit(TaskModel model, AuthInfo authInfo)
-        => authInfo.Roles.Contains((int)Role.IntranetUser)
-           && model.Status == TaskStatus.Draft ||
-           authInfo.Roles.Contains((int)Role.LegalHead)
-           && model.Status == TaskStatus.New;
-
+        => (authInfo.Roles.Contains((int)Role.IntranetUser)
+           && model.Status == TaskStatus.Draft && model.AuthorUserProfileId == authInfo.UserProfileId) 
+            || authInfo.Roles.Contains((int)Role.LegalHead) && model.Status == TaskStatus.New;
 
     private static bool CanDelete(TaskModel model, AuthInfo authInfo)
     {
