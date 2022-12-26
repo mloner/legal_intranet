1ce2953b38b88dfab52cad75e4bbc660144b6cde
Nikita Romashov <rmshvnkt@yandex.ru>
2022-12-26 11:15:02 +0400

    UTG-2427_forbid_create_subtask_for_subtask

--- src/Utg.LegalService.Common/Models/Client/Task/TaskAccessRights.cs
+++ src/Utg.LegalService.Common/Models/Client/Task/TaskAccessRights.cs
@@ -2,13 +2,14 @@
 {
     public class TaskAccessRights
     {
-        public bool CanShowDetails { get; set; }
-        public bool CanEdit { get; set; }
-        public bool CanDelete { get; set; }
-        public bool CanMakeReport { get; set; }
-        public bool CanPerform { get; set; }
-        public bool CanReview { get; set; }
-        public bool HasShortCycle { get; set; }
-        public bool CanMoveToDone { get; set; }
+        public bool? CanShowDetails { get; set; }
+        public bool? CanEdit { get; set; }
+        public bool? CanDelete { get; set; }
+        public bool? CanMakeReport { get; set; }
+        public bool? CanPerform { get; set; }
+        public bool? CanReview { get; set; }
+        public bool? HasShortCycle { get; set; }
+        public bool? CanMoveToDone { get; set; }
+        public bool? CanCreateSubtask { get; set; }
     }
 }
